6d70a25429eac228180c98bbe380f5851b3e1434
Deevanshik <137674395+Deevanshik@users.noreply.github.com>
2024-12-02 21:50:25 +0530

    Update finetune_sdm.sh

--- finetune_sdm.sh
+++ finetune_sdm.sh
@@ -5,5 +5,5 @@ python main.py \
     --scale_lr False \
     --num_nodes 1 \
     --check_val_every_n_epoch 1 \
-    --finetune_from /kaggle/input/sdm-weights-original/sd-v1-4-full-ema.ckpt
+    --finetune_from /kaggle/input/sdm-weights-original/sd-v1-4-full-ema.ckpt \
     --precision 16
